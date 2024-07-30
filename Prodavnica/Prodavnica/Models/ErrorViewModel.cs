0350051fa4d1428a371f0711fa718f3cd45eea89
BogdanZivkovic2001 <bogdanzv2001@gmail.com>
2024-07-30 17:58:31 +0200

    Kreiranje projekta

---
+++ Prodavnica/Prodavnica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prodavnica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

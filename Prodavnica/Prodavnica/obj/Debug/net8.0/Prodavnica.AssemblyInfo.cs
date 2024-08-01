09919bb8f56d6a8f956f59aabe97808fd0d2d8b0
BogdanZivkovic2001 <bogdanzv2001@gmail.com>
2024-08-01 15:43:15 +0200

    Dodavanje Entity Framework paketa

--- Prodavnica/Prodavnica/obj/Debug/net8.0/Prodavnica.AssemblyInfo.cs
+++ Prodavnica/Prodavnica/obj/Debug/net8.0/Prodavnica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prodavnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d228b87616a70859efc5944a6fabd37844b3c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prodavnica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prodavnica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

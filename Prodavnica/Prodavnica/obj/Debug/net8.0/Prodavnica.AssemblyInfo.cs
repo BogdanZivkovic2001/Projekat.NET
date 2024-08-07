5a55dbd97cc6498450c3a1ed3d35e2248e7bbee7
BogdanZivkovic2001 <bogdanzv2001@gmail.com>
2024-08-07 15:20:30 +0200

    Home stranica
    Dodavanje hero section i dela za prikazivanje najnovijih prioizvoda

--- Prodavnica/Prodavnica/obj/Debug/net8.0/Prodavnica.AssemblyInfo.cs
+++ Prodavnica/Prodavnica/obj/Debug/net8.0/Prodavnica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prodavnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b5e8dff4dfc64ef0307a69cbefa346c6f2a9b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d2c5287145e85a47f43c9aff5ae75af5635632e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prodavnica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prodavnica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
